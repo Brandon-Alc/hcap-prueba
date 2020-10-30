954327724eeeb6040871f254bb1918f72045fe7d
Brandon-Alc <brandon@MacBook-Air-de-Brandon.local>
2020-10-29 23:25:33 -0600

    Actualización

---
+++ miconvolucion.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def convolucion(A,B):
+    c=0
+    return C
+
+Matriz1=[[6,9,0,3],[8,4,9,1],[4,1,3,12],[3,2,1,100]]
+Filtro=[[1,0,2],[5,0,9],[6,2,1]]
+
+A=np.array(Matriz1)
+B=np.array(Filtro)
+
+C=np.zeros((2,2))
+
+
+print(A)
+
+print(A[1][0])
+
+print(C)
+
